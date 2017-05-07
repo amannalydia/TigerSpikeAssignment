29e4c6648856afff141aa1a27fcae2ef95c90a00
Lydia Amanna <lydiaamanna@Lydias-MacBook-Pro.local>
2017-05-07 15:57:01 +0400

    Created basic splash activity

---
+++ app/src/main/java/com/tigerspike/assignment/view/SplashScreenActivity.java
@@ -0,0 +1,34 @@
+package com.tigerspike.assignment.view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tigerspike.assignment.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private Timer splash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        splash = new Timer();
+        splash.schedule(new Splash(),3000);
+    }
+
+    private class Splash extends TimerTask {
+
+        @Override
+        public void run() {
+            startActivity(new Intent(SplashScreenActivity.this,FlickrFeedActivity.class));
+            splash.cancel();
+            splash.purge();
+            finish();
+
+        }
+    }
+}
